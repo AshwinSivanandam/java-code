0dc53bc673eee62a5e29085844cfe6afde773a02
AshwinSivanandam <46950563+AshwinSivanandam@users.noreply.github.com>
2024-10-01 12:16:36 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class demo {
     public static void main(String[] args) {
-        // Print a message to the console as outputt.
+        // Print a message to the console as outputt..
         System.out.println("Hello welcome to devops");
     }
 }
